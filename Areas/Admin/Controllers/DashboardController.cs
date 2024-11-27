2f2df84db088c4933911564052d732cd0f5f08b5
Lethal15004 <Phamvahuy2004@gmail.com>
2024-11-27 18:57:23 +0700

    Đăng nhập Admin, Giao diện cơ bản Admin

---
+++ Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChodoidoUTE.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class DashboardController : Controller
+    {
+        [Route("/admin/dashboard")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ wwwroot/js/Admin/main.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    const $userAdmin = $('[userAdminAvatar]');
+    const $userAdminLi = $('[userAdminLi]');
+    if ($userAdmin) {
+        $userAdmin.on('click', function (e) {
+            e.preventDefault();
+            const $showLiItem = $userAdminLi.find('[userAdminDropdown]');
+            $showLiItem.toggleClass('show');
+        });
+    }
+});
